a0696074ddc77fc34b0e781f9773b4b94065ae9d
cryo <bhamon.cryo@gmail.com>
2016-10-20 21:26:58 +0200

    Keys must be loaded by hand.

--- config.sample/tokens.js
+++ config.sample/tokens.js
@@ -2,7 +2,8 @@
 
 const lib = {
 	node:{
-		path:require('path')
+		path:require('path'),
+		fs:require('fs')
 	}
 };
 
@@ -12,7 +13,7 @@ const lib = {
 module.exports.auth = {};
 module.exports.auth.algorithm = 'ES512';
 module.exports.auth.keys = {};
-module.exports.auth.keys.public = lib.node.path.resolve(__dirname, '..', 'keys', 'auth.pub.pem');
+module.exports.auth.keys.public = lib.node.fs.readFileSync(lib.node.path.resolve(__dirname, '..', 'keys', 'auth.pub.pem')).toString();
 module.exports.auth.version = 'speculos-auth@1';
 
 /**
@@ -21,6 +22,6 @@ module.exports.auth.version = 'speculos-auth@1';
 module.exports.self = {};
 module.exports.self.algorithm = 'ES512';
 module.exports.self.keys = {};
-module.exports.self.keys.public = lib.node.path.resolve(__dirname, '..', 'keys', 'self.pub.pem');
-module.exports.self.keys.private = lib.node.path.resolve(__dirname, '..', 'keys', 'self.pkey.pem');
+module.exports.self.keys.public = lib.node.fs.readFileSync(lib.node.path.resolve(__dirname, '..', 'keys', 'self.pub.pem')).toString();
+module.exports.self.keys.private = lib.node.fs.readFileSync(lib.node.path.resolve(__dirname, '..', 'keys', 'self.pkey.pem')).toString();
 module.exports.self.version = 1;
